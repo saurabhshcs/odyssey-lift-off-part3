dfe6b33316c424bd65f417ff2d94e64fe9afc285
saurabhshcs <saurabhshcs@gmail.com>
2022-10-29 01:40:16 +0100

    Intial code commit

---
+++ server/src/datasources/track-api.js
@@ -0,0 +1,23 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class TrackAPI extends RESTDataSource {
+  constructor() {
+    super();
+    // the Catstronauts catalog is hosted on this server
+    this.baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
+  }
+
+  getTracksForHome() {
+    return this.get('tracks');
+  }
+
+  getAuthor(authorId) {
+    return this.get(`author/${authorId}`);
+  }
+
+  getTrack(id) {
+    return this.get(`track/${id}`);
+  }
+}
+
+module.exports = TrackAPI;
